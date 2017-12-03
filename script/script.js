a049d0c3de953e5cef0f6d53e9c9e28f404abc7f
lubomirvolcko <lubomir.volcko@akademiasovy.sk>
2017-12-03 20:30:19 +0100

    flipping card back

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
-var player=1;
+var player = 1;
+var turn1 = -1;
+var turn2 = -1;
 var field = [ [1,1,2,2,], [3,3,4,4,], [5,5,6,6], [7,7,8,8] ];
 console.log(field);
 shuffle();
@@ -16,8 +18,7 @@ function shuffle() {
             field[r1][c1] = field[r2][c2];
             field[r2][c2] = temp;
         }
-    console.log("shuffle finished! "+field);
-       
+    console.log("shuffle finished! "+field); 
 }
 
 function clickImg(value) {
@@ -25,16 +26,48 @@ function clickImg(value) {
     var col=Math.floor(value%10);
     
     console.log("you clicked: row: "+row+" col: "+col);    
-    
-    
-    
-    if(field[row][col]>0){
-        console.log("Flip card");  
+       
+    if(field[row][col]>0){  
         var source="img/img"+field[row][col]+".jpg";
-        var image="img"+row+col;
-        console.log(source);
-        console.log(image);
-        document.getElementById(image).src=source;
-        
+        var imageId="img"+row+col;
+        console.log(imageId);
+        document.getElementById(imageId).src=source;        
     }
+	if(turn1==-1) //flip first card
+		turn1 = row*10+col;
+	else {
+		turn2 = row*10+col;
+		chceckSelectedCards();
+	}
+}
+
+function chceckSelectedCards () {
+	var row1=Math.floor(turn1/10);
+	var col1=turn1%10;
+	var row2=Math.floor(turn2/10);
+	var col2=turn2%10;
+	
+	if(field[row1][col1]==field[row2][col2]) {
+		//correct
+	}else {
+		// uncorrect
+		setTimeout(turnBack,3000);
+	}
+}
+
+function turnBack() {
+	console.log("test");
+	var row1=Math.floor(turn1/10);
+	var col1=turn1%10;
+	var row2=Math.floor(turn2/10);
+	var col2=turn2%10;
+	var imageId="img"+row1+col1;
+	document.getElementById(imageId).src="img/pexeso_logo.jpeg";
+	console.log(imageId);
+	imageId="img"+row2+col2;
+	console.log("testA "+imageId);
+	document.getElementById(imageId).src="img/pexeso_logo.jpeg";
+	console.log("testB "+imageId);
+		turn1 = -1;
+		turn2 = -1;
 }
